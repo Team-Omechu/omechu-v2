f5011560884ec170e65cec2664a696c69effb938
jeonbinggu <tinyjbk123@gmail.com>
2026-02-23 16:54:59 +0900

    refactor: 맛집 카드 클릭 제거-#312

--- omechu-app/src/app/(public)/mainpage/result/[menuId]/page.tsx
+++ omechu-app/src/app/(public)/mainpage/result/[menuId]/page.tsx
@@ -293,9 +293,6 @@ export default function MenuDetailPage() {
                   address={item.formattedAddress}
                   price={item.priceLevel}
                   image={imageSrc}
-                  onCardClick={() =>
-                    router.push(`/restaurant/restaurant-detail/${item.id}`)
-                  }
                 />
               );
             })}
